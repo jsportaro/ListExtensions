9d6a65aed1746a731f234b16f4b364a6c8a77ebe
joseph <joseph@DESKTOP-CD605HV>
2017-02-26 21:07:09 -0500

    Add project files.

---
+++ ListExtensions.Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ListExtensions.Tests
+{
+
+    public class EnumerableExtensionsTests
+    {
+        class StringModel
+        {
+            public string Name;
+        }
+
+        [Fact]
+        public void Should_Create_String_Column()
+        {
+            var listOfStringModels = new List<StringModel>()
+            {
+            };
+
+            var table = listOfStringModels.ToDataTable();
+
+            Assert.True(table.Columns[0].DataType == typeof(string));
+        }
+
+        [Fact]
+        public void Should_Create_New_Row_For_Each_Item_In_Enumerable()
+        {
+            var listOfStringModels = new List<StringModel>()
+            {
+                new StringModel() { Name = "Name" }
+            };
+
+            var table = listOfStringModels.ToDataTable();
+
+            Assert.True(table.Rows.Count == listOfStringModels.Count);
+        }
+
+        [Fact]
+        public void Should_Set_New_Row_Columns_To_Items_Field_Values()
+        {
+            var name = "Name";
+            var listOfStringModels = new List<StringModel>()
+            {
+                new StringModel() { Name = name}
+            };
+
+            var table = listOfStringModels.ToDataTable();
+
+            Assert.True((table.Rows[0]["Name"] as string) == name);
+        }
+
+        [Fact]
+        public void Should_Return_Empty_Table_When_List_Is_Empty()
+        {
+            var listOfStringModels = new List<StringModel>()
+            {
+            };
+
+            var table = listOfStringModels.ToDataTable();
+
+            Assert.Empty(table.Rows);
+        }
+
+        class BigModel
+        {
+            public long Id;
+            public string Name;
+            public long OtherId;
+            public string AnotherName;
+            public long ReferencedId;
+            public string ReferencedName;
+        }
+
+        [Fact]
+        public void Stress_Test()
+        {
+            var list = new List<BigModel>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                list.Add(new BigModel()
+                {
+                    Id = i,
+                    Name = "Name",
+                    OtherId = i + 1,
+                    AnotherName = "AnotherName",
+                    ReferencedId = i + 2,
+                    ReferencedName = "ReferencedName"
+                });
+            }
+
+            var table = list.ToDataTable();
+
+            Assert.Equal(10000, table.Rows.Count);
+
+        }
+    }
+}
---
+++ ListExtensions/EnumerableExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExtensions
+{
+    public static class EnumerableExtensions
+    {
+        public static DataTable ToDataTable<T>(this IEnumerable<T> enumerable)
+        {
+            var dataTable = new DataTable();
+
+            if (enumerable == null)
+                throw new ArgumentNullException("enumerable");
+
+            if (enumerable.GroupBy(i => i.GetType()).Count() > 1)
+                throw new ArgumentException($"Can only table enumerables of the same type");
+
+            var enumerableType = typeof(T);
+            
+            foreach (var field in enumerableType.GetFields())
+            {
+                dataTable.Columns.Add(new DataColumn()
+                {
+                    DataType = field.FieldType,
+                    ColumnName = field.Name
+                });
+            }
+
+            foreach (var item in enumerable)
+            {
+                var row = dataTable.NewRow();
+
+                foreach (var field in enumerableType.GetFields())
+                {
+                    row[field.Name] = field.GetValue(item);
+                }
+
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
+        }
+    }
+}
